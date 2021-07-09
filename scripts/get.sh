2fbd805b34f3c6642291f56552d4e736705433e2
soroka.dmitry <krafft1981@mail.ru>
2021-07-10 03:19:30 +0400

    bug fix

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 user=krafft1981@mail.ru
-pass=1234567890
+pass=12345Q
 
 #url="http://127.0.0.1:8080"
 url="http://138.124.187.10:8080"
 
-create="yes"
+create=""
 
 if [ ! -z $create ]
 then
@@ -15,34 +15,14 @@ then
     curl -X POST "$url/parking" --digest --user "$user:$pass" -H "accept: */*"
 
 else
-    curl -X POST "$url/transport?type=1" --digest --user "$driver1:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver2:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver3:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver4:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver5:$p1"
-
-    curl -X POST "$url/transport?type=1" --digest --user "$driver1:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver2:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver3:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver4:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver5:$p1"
-
-    curl -X POST "$url/transport?type=1" --digest --user "$driver1:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver2:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver3:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver4:$p1"
-    curl -X POST "$url/transport?type=1" --digest --user "$driver5:$p1"
-
-    curl -X POST "$url/transport?type=1" --digest --user "$customer1:$p1"
-    curl -X POST "$url/transport?type=2" --digest --user "$customer1:$p1"
-    curl -X POST "$url/transport?type=3" --digest --user "$customer1:$p1"
-    curl -X POST "$url/transport?type=4" --digest --user "$customer1:$p1"
-
-    curl -X POST "$url/transport?type=1" --digest --user "$customer2:$p2"
-    curl -X POST "$url/transport?type=4" --digest --user "$customer2:$p2"
-
-    curl -X POST "$url/transport?type=1" --digest --user "$customer3:$p1"
-    curl -X POST "$url/transport?type=2" --digest --user "$customer3:$p1"
-    curl -X POST "$url/transport?type=3" --digest --user "$customer3:$p1"
-    curl -X POST "$url/transport?type=4" --digest --user "$customer3:$p1"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
+    curl -X POST "$url/transport?type=1" --digest --user "$user:$pass"
 fi
