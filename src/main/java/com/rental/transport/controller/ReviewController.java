eaee1ce00fd85e29ea2f3255314d9556d2a2694d
Сорока Дмитрий Николаевич (4038347) <soroka@dev.vtb.ru>
2021-11-24 07:30:25 +0400

    Разное

--- src/main/java/com/rental/transport/controller/ReviewController.java
+++ src/main/java/com/rental/transport/controller/ReviewController.java
@@ -1,8 +1,10 @@
 package com.rental.transport.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rental.transport.service.ReviewService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,39 +20,51 @@ import java.util.Map;
 public class ReviewController {
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private ReviewService service;
 
     @ApiOperation(
         value = "Добавление отзыва"
     )
     @PostMapping
-    public Map<String, Long> doPostReview(
+    public ResponseEntity<Map<String, Long>> doPostReview(
         Principal principal,
         @RequestParam(value = "transport_id", required = true) Long transportId,
-        @RequestParam(value = "score", required = true) Long score) {
-
-        return service.addReview(principal.getName(), transportId, score);
+        @RequestParam(value = "score", required = true) Long score) throws Exception {
+        Map<String, Long> response =  service.addReview(principal.getName(), transportId, score);
+        return ResponseEntity
+                   .ok()
+                   .contentLength(objectMapper.writeValueAsBytes(response).length)
+                   .body(response);
     }
 
     @ApiOperation(
         value = "Удаление отзыва"
     )
     @DeleteMapping
-    public Map<String, Long> doDeleteReview(
+    public ResponseEntity<Map<String, Long>> doDeleteReview(
         Principal principal,
-        @RequestParam(value = "transport_id", required = true) Long transportId) {
-
-        return service.delReview(principal.getName(), transportId);
+        @RequestParam(value = "transport_id", required = true) Long transportId) throws Exception {
+        Map<String, Long> response = service.delReview(principal.getName(), transportId);
+        return ResponseEntity
+                   .ok()
+                   .contentLength(objectMapper.writeValueAsBytes(response).length)
+                   .body(response);
     }
 
     @ApiOperation(
         value = "Получение отзывов"
     )
     @GetMapping
-    public Map<String, Long> doGetReview(
+    public ResponseEntity<Map<String, Long>> doGetReview(
         Principal principal,
-        @RequestParam(value = "transport_id", required = true) Long transportId) {
-
-        return service.getScore(principal.getName(), transportId);
+        @RequestParam(value = "transport_id", required = true) Long transportId) throws Exception {
+        Map<String, Long> response = service.getScore(principal.getName(), transportId);
+        return ResponseEntity
+                   .ok()
+                   .contentLength(objectMapper.writeValueAsBytes(response).length)
+                   .body(response);
     }
 }
